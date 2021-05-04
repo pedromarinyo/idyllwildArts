d54b0255e599a50e0f42707d999c88ac1f9d4d88
Pedro <pedrosilva0219@gmail.com>
2021-05-04 16:23:23 -0400

    Changed to PHP

---
+++ commentGenerator/scraper.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>Progress Tracker</title>	
+</head>
+
+
+
+<body>
+	<div id="output">
+
+		<?php
+			
+			if (($handle = fopen("data.csv", "r")) !== FALSE) {
+			  while (($data = fgetcsv($handle, 1000, "\n")) !== FALSE) {
+
+			    for ($c=0; $c < count($data); $c++) {
+
+			    	$firstName = explode(",", $data[$c])[0];
+			    	$lastName = explode(",", $data[$c])[1];
+			    	$knowre = explode(",", $data[$c])[3];
+
+			    	echo "<div id='" . $lastName . ", " . $firstName . "'>" . $knowre . "</div>";			        
+			    }
+			  }
+			  fclose($handle);
+			}
+		?>
+
+	</div>
+</body>
+
+</html>
